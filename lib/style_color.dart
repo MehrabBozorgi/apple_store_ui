91781a2bedbc213ef2213da9f875d666f9a35a37
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-05-07 10:15:26 +0430

    apple_store_ui

---
+++ lib/style_color.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor = Color(0xFF222224);
+const detailBackGroundColor = Color(0xFF131313);
+const buttonColor = Color(0xFF1372E0);
+const color = Color(0xFFE7E7E7);
+const iconColor = Color(0xFF5F5F5F);
+
+const bigTitleStyle = TextStyle(
+  color: color,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1,
+);
+const nameStyle = TextStyle(
+  color: color,
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+const descriptionStyle = TextStyle(
+    color: color,
+  fontSize: 14,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 1,
+  height: 1.5
+);
+const titleStyle = TextStyle(
+  color: color,
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+const unSelectTitleStyle = TextStyle(
+  color: iconColor,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
